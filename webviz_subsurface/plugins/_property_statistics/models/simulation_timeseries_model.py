3b1245047d236832e5bf6e278ed7df800c7721a6
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2020-12-02 11:25:44 +0100

    Type hints for property_statistics plugin

--- webviz_subsurface/plugins/_property_statistics/models/simulation_timeseries_model.py
+++ webviz_subsurface/plugins/_property_statistics/models/simulation_timeseries_model.py
@@ -2,6 +2,7 @@ from typing import Optional, Union
 
 import pandas as pd
 from webviz_config.common_cache import CACHE
+from webviz_config import WebvizConfigTheme
 
 from webviz_subsurface._abbreviations.reservoir_simulation import (
     simulation_vector_description,
@@ -20,7 +21,7 @@ class SimulationTimeSeriesModel:
     def __init__(
         self,
         dataframe: pd.DataFrame,
-        theme: dict = None,
+        theme: WebvizConfigTheme,
         metadata: Optional[pd.DataFrame] = None,
         line_shape_fallback: str = "linear",
     ) -> None:
@@ -77,7 +78,7 @@ class SimulationTimeSeriesModel:
         return self._metadata
 
     @property
-    def vectors(self):
+    def vectors(self) -> list:
         return [
             c
             for c in self.dataframe.columns
@@ -145,15 +146,12 @@ class SimulationTimeSeriesModel:
                     self.get_line_shape(vector),
                 )
             )
-        if (
-            historical_vector(vector=vector, smry_meta=self.metadata)
-            in dataframe.columns
-        ):
-            traces.append(
-                self.add_history_trace(
-                    dataframe, historical_vector(vector=vector, smry_meta=self.metadata)
-                )
-            )
+
+        historical_vector_name = historical_vector(
+            vector=vector, smry_meta=self.metadata
+        )
+        if historical_vector_name and historical_vector_name in dataframe.columns:
+            traces.append(self.add_history_trace(dataframe, historical_vector_name))
         return traces
 
     def add_history_trace(self, dframe: pd.DataFrame, vector: str) -> dict:
@@ -200,15 +198,11 @@ class SimulationTimeSeriesModel:
             for real_idx, (real, real_df) in enumerate(ens_df.groupby("REAL"))
         ]
 
-        if (
-            historical_vector(vector=vector, smry_meta=self.metadata)
-            in dataframe.columns
-        ):
-            traces.append(
-                self.add_history_trace(
-                    dataframe, historical_vector(vector=vector, smry_meta=self.metadata)
-                )
-            )
+        historical_vector_name = historical_vector(
+            vector=vector, smry_meta=self.metadata
+        )
+        if historical_vector_name and historical_vector_name in dataframe.columns:
+            traces.append(self.add_history_trace(dataframe, historical_vector_name))
         return traces
 
     @CACHE.memoize(timeout=CACHE.TIMEOUT)
@@ -237,20 +231,16 @@ class SimulationTimeSeriesModel:
             for real_idx, (real, real_df) in enumerate(dataframe.groupby("REAL"))
         ]
 
-        if (
-            historical_vector(vector=vector, smry_meta=self.metadata)
-            in dataframe.columns
-        ):
-            traces.append(
-                self.add_history_trace(
-                    dataframe, historical_vector(vector=vector, smry_meta=self.metadata)
-                )
-            )
+        historical_vector_name = historical_vector(
+            vector=vector, smry_meta=self.metadata
+        )
+        if historical_vector_name and historical_vector_name in dataframe.columns:
+            traces.append(self.add_history_trace(dataframe, historical_vector_name))
         return traces
 
 
 def add_fanchart_traces(
-    vector_stats: pd.DataFrame, color: str, legend_group: str, line_shape
+    vector_stats: pd.DataFrame, color: str, legend_group: str, line_shape: str
 ) -> list:
     """Renders a fanchart for an ensemble vector"""
 
--- webviz_subsurface/plugins/_property_statistics/views/property_qc_view.py
+++ webviz_subsurface/plugins/_property_statistics/views/property_qc_view.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 import dash_html_components as html
 import dash_core_components as dcc
 import webviz_core_components as wcc
@@ -9,8 +11,11 @@ from .selector_view import (
     source_selector,
 )
 
+if TYPE_CHECKING:
+    from .. import PropertyStatistics
 
-def selector_view(parent) -> html.Div:
+
+def selector_view(parent: "PropertyStatistics") -> html.Div:
     return html.Div(
         className="framed",
         style={"height": "80vh", "overflowY": "auto"},
@@ -32,7 +37,7 @@ def selector_view(parent) -> html.Div:
     )
 
 
-def property_qc_view(parent) -> wcc.FlexBox:
+def property_qc_view(parent: "PropertyStatistics") -> wcc.FlexBox:
     return wcc.FlexBox(
         style={"margin": "20px"},
         children=[
