ba683706abe512083df97887875014d6d50d791a
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2021-04-20 08:53:29 +0200

    Update draft_simulation_timeseries_model.pyi

--- webviz_subsurface/_models/draft_simulation_timeseries_model.pyi
+++ webviz_subsurface/_models/draft_simulation_timeseries_model.pyi
@@ -1,54 +1,38 @@
-from typing import List, Dict, Optional, Iterable, Tuple, Sequence
+from typing import List, Optional, Sequence
+import datetime
+import abc
 
 import pandas as pd
-import numpy as np
 
 # fmt: off
 
+# Class provides data for ensemble summary (timeseries) data
 
+# Alternate class names:
+#   EnsembleSummaryProvider
+#   EnsembleSummaryAccess
+#   EnsembleSummaryDataSource
+#   EnsembleSummaryDataProvider
+#   EnsembleSummaryDataAccess
+#   EnsembleTimeSeries
 
-class SimulationTimeSeriesModel:
-    def __init__(self, smry_df: pd.DataFrame, smry_meta_df: Optional[pd.DataFrame] = None) -> None: ...
-    
-    @property
-    #def ensemble_names(self) -> List[str]: ...
-
-    # Queries for meta data
+class EnsembleSummaryProvider(abc.ABC):
+    @abc.abstractmethod
     def vector_names(self) -> List[str]: ...
-    def has_vector_named(vector_name: str) -> bool: ...
-    def get_filtered_vector_names(self, excludeAllZeroVals: bool, excludeConstantVal: bool) -> List[str]: ...
 
+    @abc.abstractmethod
+    def vector_names_filtered_by_value(self, exclude_all_values_zero: bool = False, exclude_constant_values: bool = False) -> List[str]: ...
+
+    @abc.abstractmethod
     def realizations(self) -> List[int]: ...
 
-    def dates(self, realizations: Optional[Sequence[int]]) -> List[str]: ...
+    @abc.abstractmethod
+    def dates(self, realizations: Optional[Sequence[int]] = None) -> List[datetime.datetime]: ...
 
-    # Data access
-    def get_vector(self, vector_name: str, realizations: Optional[Sequence[int]]) -> pd.DataFrame: ...
-    def get_vectors(self, vector_names: List[str], realizations: Optional[Sequence[int]]) -> pd.DataFrame: ...
+    @abc.abstractmethod
+    def get_vectors_df(self, vector_names: Sequence[str], realizations: Optional[Sequence[int]] = None) -> pd.DataFrame: ...
 
-    def get_vectors_for_date(self, date: str, vector_names: Sequence[str]) -> pd.DataFrame: ...
-    def get_vectors_for_date(self, date: str, vector_names: Sequence[str], realizations: Optional[Sequence[int]]) -> pd.DataFrame: ...
-
-    # If we were to have this method, what is the definition of "all" vectors
-    def get_all_vectors_for_date(self, date: str, realizations: Optional[Sequence[int]]) -> pd.DataFrame: ...
-
-
-    # Still needs to be discussed
-    #   Data type for dates?
-    #   Is DataFrame the best data structure for return values?
-    #   Query for the existence of a vector by name, eg.: has_vector_named()
-
-
-# Return values consisting of numpy arrays instead of Pandas DataFrames
-# Alernative class name RealizationVectors
-#@dataclass
-class RealizationData:
-    realization: int  
-    date_array: np.ndarray[np.str_]
-    vector_names: List[str]
-    vector_arrays: List[np.ndarray[np.float32]]
-
-# and then
-def get_vectors(self, vector_names: List[str], realizations: Optional[Sequence[int]]) -> List[RealizationData]: ...
+    @abc.abstractmethod
+    def get_vectors_for_date_df(self, date: datetime.datetime, vector_names: Sequence[str], realizations: Optional[Sequence[int]] = None) -> pd.DataFrame: ...
 
 # fmt: on
